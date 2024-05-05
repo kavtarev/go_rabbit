40231bda90c399acc1329b6fc20fe987304754d1
ivan kavtarev <ivan.kavtarev@mail.ru>
2024-05-05 22:38:01 +0300

    init jwt

--- api.go
+++ api.go
@@ -3,7 +3,11 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
 )
 
 type Api struct {
@@ -16,6 +20,49 @@ func toJson(w http.ResponseWriter, status int, v any) error {
 	w.WriteHeader(status)
 
 	return json.NewEncoder(w).Encode(v)
+}
+
+func JWTAccess(fn http.HandlerFunc) http.HandlerFunc{
+	return func (w http.ResponseWriter, r *http.Request) {
+		fmt.Println("in jwt")
+		token := r.Header.Get("jwt_token")
+		t, err := validateJWT(token)
+		fmt.Println("Header", t.Header)
+		fmt.Println("Claims", t.Claims)
+		fmt.Println("Signature", t.Signature)
+		fmt.Println("Valid", t.Valid)
+		if err != nil {
+			toJson(w, http.StatusBadRequest, "goooooooovna")
+			return
+		}
+
+		fn(w,r)
+	}
+}
+const secret = "some_secret"
+
+func createJWT(id string) (string, error){
+	claims := jwt.MapClaims{
+		"sub":  id,
+		"exp":  time.Now().Add(time.Second * 10).Unix(),
+}
+	
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
+
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+	
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(secret), nil
+	})
+	
 }
 
 
@@ -38,9 +85,10 @@ func (api *Api) Run() {
 	api.storage.Init()
 	server := http.NewServeMux()
 
-	server.HandleFunc("/", MapHandlers(api.Some))
+	server.HandleFunc("/", JWTAccess(MapHandlers(api.Some)))
 	server.HandleFunc("/create", MapHandlers(api.CreateUser))
 	server.HandleFunc("/user", MapHandlers(api.GetUserById))
+	server.HandleFunc("/token", MapHandlers(api.CreateToken))
 
 	http.ListenAndServe(api.address, server)
 }
@@ -48,6 +96,16 @@ func (api *Api) Run() {
 func (api *Api) Some(w http.ResponseWriter, r *http.Request) error {
 	toJson(w, http.StatusOK, "it's all good, man")
 	return nil
+}
+
+func (api *Api) CreateToken(w http.ResponseWriter, req *http.Request) error {
+	token, err := createJWT("some")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(token)
+
+	return nil;
 }
 
 func (api *Api) CreateUser(w http.ResponseWriter, req *http.Request) error {
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go_rabbit
 
 go 1.21.0
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)

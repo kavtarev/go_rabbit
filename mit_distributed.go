204b437b810dce527ea56be5c5e03071412a1da9
ivan kavtarev <ivan.kavtarev@mail.ru>
2024-06-10 10:45:11 +0300

    async

--- mit_distributed.go
+++ mit_distributed.go
@@ -3,13 +3,16 @@ package main
 import (
 	"errors"
 	"fmt"
+	"sync"
 )
 
 func main() {
 	store := Store{dict: make(map[string][]string)}
 	store.SetInitDict()
-
+	fmt.Println("-------sync-go----------")
 	syncFetch("first", store, make(map[string]bool))
+	fmt.Println("-------async-go----------")
+	asyncFetch("first", store, &asyncGo{dict: make(map[string]bool)})
 }
 
 func syncFetch(url string, store Store, dict map[string]bool) {
@@ -24,18 +27,47 @@ func syncFetch(url string, store Store, dict map[string]bool) {
 	}
 
 	for _, url := range res {
-		fmt.Println(url)
 		syncFetch(url, store, dict)
 	}
-
 }
 
+func asyncFetch(url string, store Store, ass *asyncGo) {
+	ass.mu.Lock()
+	if _, ok := ass.dict[url]; ok {
+		ass.mu.Unlock()
+		return
+	}
+	ass.dict[url] = true
+	ass.mu.Unlock()
+
+	res, err := store.Fetch(url)
+	if err != nil {
+		fmt.Println("error fetching")
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(res))
+
+	for _, url := range res {
+		go func(url string) {
+			defer wg.Done()
+			asyncFetch(url, store, ass)
+		}(url)
+	}
+	wg.Wait()
+}
+
+type asyncGo struct {
+	mu   sync.Mutex
+	dict map[string]bool
+}
 type Store struct {
 	dict map[string][]string
 }
 
 func (d Store) Fetch(url string) ([]string, error) {
 	if res, ok := d.dict[url]; ok {
+		fmt.Printf("found:   %s\n", url)
 		return res, nil
 	}
 
